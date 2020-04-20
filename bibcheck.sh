e28a6c5a6af416a71c25b4daa2a51dc702716f43
poggioenzo <poggio.enzo@gmail.com>
2020-04-20 09:57:02 -0400

    simplify code and remove newline

--- bibcheck.sh
+++ bibcheck.sh
@@ -10,15 +10,15 @@ TMP='/tmp/bibcheck.bib'
 # If there is no arg take user input with gedit,
 # Else use the first arg as input file.
 [ $# -eq 0 ] && (rm $TMP 2>&-; gedit $TMP 2>&- ; true) || TMP=$1;
-echo -e $RED;
+echo -e -n $RED;
 # Check if the file exists.
-[ -f $TMP ] || echo -e "File $TMP does not exist" $RESET && false &&
+[ -f $TMP ] || echo -e -n "File $TMP does not exist" $RESET &&
 # Lint errors and redirect output in a .checked file.
 bibclean $TMP 1> $TMP.checked || false &&
 # Set clipboard with pretify result if no error.
 xclip -selection clipboard < $TMP.checked &&
 # Print message and remove working files.
 echo -e $GREEN "Linted and pretify version to copy to clipboard.";
-echo -e $RESET;
+echo -e -n $RESET;
 [ $# -eq 0 ] && rm $TMP 2>&-
 rm $TMP.checked 2>&-
